252dcd0453a12f9ea942474cedad7b94e7666ee0
Sebastian Rollen <seb@overmu.se>
2021-11-15 12:17:58 -0500

    feature: Use vila as the request library to improve pagination

--- src/rest/mod.rs
+++ src/rest/mod.rs
@@ -1,4 +1,5 @@
 use vila::Client;
+mod date_utils;
 pub mod reference;
 pub mod stocks;
 
--- src/ws/mod.rs
+++ src/ws/mod.rs
@@ -241,8 +241,8 @@ mod test {
 
         con_rx.await.expect("Server not ready");
         let connection = Connection::new(
-            "ws://localhost:12345".into(),
-            "test".into(),
+            "ws://localhost:12345",
+            "test",
             &["T", "Q", "A", "AM"],
             &["AAPL", "TSLA"],
         );
